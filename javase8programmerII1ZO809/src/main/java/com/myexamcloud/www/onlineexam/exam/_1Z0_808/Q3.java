bb21f319114b1d1ebab4866106dbb7f74cef593d
ikka <belgampaul@gmail.com>
2016-03-29 03:39:28 +0300

    added all questions, generated from html src. each question has its ownmd file. all questions are concatenated as one file to make them available in one file

---
+++ javase8programmerII1ZO809/src/main/java/com/myexamcloud/www/onlineexam/exam/_1Z0_808/Q3.java
@@ -0,0 +1,16 @@
+package com.myexamcloud.www.onlineexam.exam._1Z0_808;
+
+public class Q3 {
+  public static void main(String[] args) {
+
+    int i = 0, j = 10;
+    try {
+      j /= i;
+    }
+//todo uncomment
+//    System.out.print("Divide by Zero! ");
+      catch(Exception e){
+      System.out.print("error");
+    }
+  }
+}
---
+++ javase8programmerII1ZO809/src/main/java/com/myexamcloud/www/onlineexam/exam/_1Z0_808/Q5.java
@@ -0,0 +1,9 @@
+package com.myexamcloud.www.onlineexam.exam._1Z0_808;
+
+public class Q5 {
+  public static void main(String[] args) {
+//todo uncomment
+//    int a[];
+//    System.out.print(a instanceof Object);
+  }
+}
